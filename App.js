d2c5e2c809171a2d2878ebffe6e59a860bea720a
arthas168 <pavlovskipetko@abv.bg>
2020-04-17 15:38:27 +0300

    Should work completely now. Without a unique key logic.

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import axios from "axios";
 import * as Contacts from "expo-contacts";
 
 const App = () => {
-  const [contacts, setContacts] = useState([]);
-
-  const getAllContacts = async () => {
+  const sendAllContactsToDB = async () => {
     const { data } = await Contacts.getContactsAsync({});
-    setContacts(data);
+    const randomSixDigitNumber = Math.floor(100000 + Math.random() * 900000);
+
+    axios
+      .post(
+        "https://us-central1-swap-contacts-server.cloudfunctions.net/addContactsList",
+        {
+          userHandle: randomSixDigitNumber,
+          contacts: data,
+        }
+      )
+      .then(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+
+    axios
+      .post(
+        "https://us-central1-swap-contacts-server.cloudfunctions.net/addToSecretNums",
+        {
+          num: randomSixDigitNumber,
+        }
+      )
+      .then(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
+
+  const loadContactsToDevice = async () => {
+    console.log("FETCHING...");
+    const res = await axios.get(
+      "https://us-central1-swap-contacts-server.cloudfunctions.net/getContacts"
+    );
+
+    const contactsFromDB = res.data[0].contacts;
+
+    contactsFromDB.map((contact) => {
+      const { contactType, firstName, name, phoneNumbers } = contact;
+
+      const contactToAdd = {
+        name: name,
+        phoneNumbers: phoneNumbers,
+        firstName: firstName,
+        contactType: contactType,
+      };
+  
+      await Contacts.addContactAsync(contactToAdd);
+  
+      console.log(`Success! ${firstName} was added to contacts.`);
+    });
+
+    // const testFirstContact = res.data[0].contacts[0];
+    // console.log(testFirstContact);
+    // const { contactType, firstName, name, phoneNumbers } = testFirstContact;
+
+    console.log("Successfully added all contacts.")
+
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Shift Contacts!</Text>
       <View style={styles.buttonContainer}>
-        <Button title="Send" onPress={getAllContacts}></Button>
-        <Button title="Get"></Button>
+        <Button title="Send" onPress={sendAllContactsToDB}></Button>
+        <Button title="Get" onPress={loadContactsToDevice}></Button>
       </View>
     </View>
   );
