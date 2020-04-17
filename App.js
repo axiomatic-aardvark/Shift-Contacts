514c122c5fee74fefe5b8d5c635349054584aa38
arthas168 <pavlovskipetko@abv.bg>
2020-04-17 08:48:24 +0300

    Added home screen. Ready for getting contacts logic.

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Welcome to Shift Contacts!</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Send"></Button>
+        <Button title="Get"></Button>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    width: 200,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 30,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize:20
   },
 });
+
+export default App;
