bd956501ffe8beaa663c23b6e94d9737e1cd8fea
arthas168 <pavlovskipetko@abv.bg>
2020-04-17 18:34:23 +0300

    Validating secret code on Get.

--- App.js
+++ App.js
@@ -5,13 +5,23 @@ import {
   View,
   Button,
   ActivityIndicator,
+  TextInput,
+  Alert,
 } from "react-native";
 import axios from "axios";
 import * as Contacts from "expo-contacts";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isInputEnabled, setIsInputEnabled] = useState(true);
   const [isDone, setIsDone] = useState(false);
+  const [uniqueNumber, setUniqueNumber] = useState("");
+
+  console.log(uniqueNumber);
+
+  const onUniqueNumberChange = (newNumber) => {
+    setUniqueNumber(newNumber);
+  };
 
   const sendAllContactsToDB = async () => {
     setIsLoading(true);
@@ -54,19 +64,76 @@ const App = () => {
     console.log(`Adding ${data.length} contacts to DB...`);
     setIsLoading(false);
     setIsDone(true);
+
+    setUniqueNumber(randomSixDigitNumber);
+    setIsInputEnabled(false);
+
+    Alert.alert(
+      `Your code is: ${randomSixDigitNumber}`,
+      "Please write down or memorize your code, you will need to enter it on the other device.",
+      [
+        {
+          text: "OK",
+          onPress: () => console.log("Ask me later pressed"),
+        },
+      ],
+      { cancelable: true }
+    );
   };
 
   const loadContactsToDevice = async () => {
+    if (uniqueNumber.length !== 6) {
+      Alert.alert(
+        `Please enter valid code!`,
+        "The code you entered does not hold reference to any contacts list in the database.",
+        [
+          {
+            text: "OK",
+            onPress: () => console.log("OK pressed"),
+          },
+        ],
+        { cancelable: true }
+      );
+      setUniqueNumber("");
+      return;
+    }
+
     setIsLoading(true);
     console.log("FETCHING...");
     const res = await axios.get(
       "https://us-central1-swap-contacts-server.cloudfunctions.net/getContacts"
     );
 
-    const contactsFromDB = res.data[0].contacts;
+    const contactsFromDB = res.data.filter((o) => {
+      console.log(
+        o.userHandle,
+        " is equal ",
+        uniqueNumber,
+        " ",
+        o.userHandle == uniqueNumber
+      );
+      return o.userHandle == uniqueNumber;
+    });
 
-    contactsFromDB.map(async (contact) => {
-      const { contactType, firstName, name, phoneNumbers } = contact;
+    if (contactsFromDB.length === 0) {
+      Alert.alert(
+        `Please enter valid code!`,
+        "The code you entered does not hold reference to any contacts list in the database.",
+        [
+          {
+            text: "OK",
+            onPress: () => console.log("OK pressed"),
+          },
+        ],
+        { cancelable: true }
+      );
+      setIsLoading(false);
+      setUniqueNumber("");
+      return;
+    }
+
+    contactsFromDB[0].contacts.map(async (contact) => {
+      const { name, phoneNumbers, firstName, contactType } = contact;
 
       const contactToAdd = {
         name: name,
@@ -75,7 +142,8 @@ const App = () => {
         contactType: contactType,
       };
 
-      await Contacts.addContactAsync(contactToAdd);
+      // await Contacts.addContactAsync(contactToAdd);
+      // console.log(contactToAdd);
 
       console.log(`Success! ${firstName} was added to contacts.`);
     });
@@ -99,12 +167,26 @@ const App = () => {
         <Text
           style={styles.success}
           onPress={() => {
+            setUniqueNumber("");
+            setIsInputEnabled(true);
             setIsDone(false);
           }}
         >
           Success! Click to go back.
         </Text>
       )}
+
+      <TextInput
+        style={styles.input}
+        blurOnSubmit
+        autoCorrect={false}
+        keyboardType="number-pad"
+        maxLength={6}
+        onChangeText={(newNumber) => onUniqueNumberChange(newNumber)}
+        value={uniqueNumber}
+        placeholder={uniqueNumber.toString() || "Your code"}
+        editable={isInputEnabled}
+      />
     </View>
   );
 };
@@ -130,10 +212,18 @@ const styles = StyleSheet.create({
     color: "green",
     fontSize: 22,
     fontWeight: "bold",
-    marginTop: 30,
+    marginVertical: 30,
   },
   loader: {
-    marginTop: 30,
+    marginVertical: 30,
+  },
+  input: {
+    width: 70,
+    textAlign: "center",
+    height: 30,
+    borderBottomColor: "grey",
+    borderBottomWidth: 1,
+    marginVertical: 10,
   },
 });
 
